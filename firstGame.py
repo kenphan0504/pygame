0f7d27743d3bb51dc5717ee910af3ab2c5111697
Ken Phan <ken.phan0504@gmail.com>
2023-03-12 16:29:57 -0700

    added jumping

--- firstGame.py
+++ firstGame.py
@@ -10,7 +10,9 @@ import pygame
 pygame.init()
 
 # creating a window (height, width) for the game 
-win = pygame.display.set_mode((500, 500))
+screen_width = 500
+screen_height = 500
+win = pygame.display.set_mode((screen_width, screen_height))
 
 # set the name for the window/game
 pygame.display.set_caption("First Game")
@@ -25,7 +27,11 @@ width = 40
 height = 50
 vel = 5 # velocity
 
+isJump = False
+jumpCount = 10
+
 while run:
+    pygame.time.delay(50)
     # handle events
     # an event in pygame is anything that happens from the user i.e a mouse click
     for event in pygame.event.get(): # get a list of all the event that happens
@@ -41,22 +47,33 @@ while run:
     # https://www.pygame.org/docs/ref/key.html#pygame.key.get_pressed
     keys = pygame.key.get_pressed()
 
-    if keys[pygame.K_LEFT]:
+    if keys[pygame.K_LEFT] and x > vel:
         x -= vel
-    if keys[pygame.K_RIGHT]:
+    if keys[pygame.K_RIGHT] and x < screen_width - width - vel:
         x += vel
-    if keys[pygame.K_UP]:
-        y -= vel
-    if keys[pygame.K_DOWN]:
-        y += vel
+    if not (isJump):
+        if keys[pygame.K_UP] and y > vel:
+            y -= vel
+        if keys[pygame.K_DOWN] and y < screen_height - height - vel:
+            y += vel
+        if keys[pygame.K_SPACE]:
+            isJump = True
+    else:
+        if jumpCount >= -10: # this is to check when to set the falling of the jump
+            neg = 1          # multiplying by negative will give positive, to go down
+            if jumpCount < 0:
+                neg = -1
+            y -= (jumpCount ** 2) * 0.5 * neg
+            jumpCount -= 1
+        else:
+            isJump = False
+            jumpCount = 10
 
     # clear display to black (0, 0, 0) for each instance of change, if fill() isn't called, the shape will "drag" instead of "move"
     win.fill((0, 0, 0))
     # draw a rectangle of color red (255,0,0) and size (width, height) on coordinates (x, y)
     # coordinates starts at top left: i.e top left (0, 0), top right (500, 0), bottom left (0, 500), bottom right (500, 500)
     pygame.draw.rect(win, (255,0, 0), (x, y, width, height))
-
-
 
     # update this display to show what's changed i.e a rectangle is drawn
     # pygame.flip() would update the entire display
